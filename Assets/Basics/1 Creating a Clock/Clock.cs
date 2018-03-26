5a6dd2a8d6c4f5d26c4ec387c87deb252fe02cbc
ben xie <mpsoft_2005@126.com>
2018-03-26 17:34:47 +0800

    creating a clock

---
+++ Assets/Basics/1 Creating a Clock/Clock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    public bool continuous = false;
+
+    const float degreePerHour = 30f;
+    const float degreePerMinute = 6f;
+    const float degreePerSecond = 6f;
+
+    void Start()
+    {
+        UpdateDiscrete();
+    }
+
+    void Update()
+    {
+        if (continuous)
+            UpdateContinuous();
+        else
+           UpdateDiscrete();
+    }
+
+    void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0, time.Hour * degreePerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, time.Minute * degreePerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, time.Second * degreePerSecond, 0);
+    }
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0, (float)time.TotalHours * degreePerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, (float)time.TotalMinutes * degreePerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, (float)time.TotalSeconds * degreePerSecond, 0);
+    }
+
+}
